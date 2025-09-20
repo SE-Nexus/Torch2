dad8b07d691cb190fd9e49ffa78b2cbc05f86007
Garrett <52760019+Casimir255@users.noreply.github.com>
2025-09-19 20:31:33 -0500

    Send test

--- IgniteSE1/Program.cs
+++ IgniteSE1/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace IgniteSE1
@@ -10,7 +12,20 @@ namespace IgniteSE1
     {
         static void Main(string[] args)
         {
+            var stdout = Console.OpenStandardOutput();
+            var writer = new StreamWriter(stdout) { AutoFlush = true };
+            Console.SetOut(writer);
+            Console.SetError(writer);
+
+            Console.WriteLine("Now this goes to Docker logs");
+
             Console.WriteLine("Hello World!");
+
+            while(true)
+            {
+                Console.WriteLine("Test");
+                Thread.Sleep(1000); 
+            }
         }
     }
 }
