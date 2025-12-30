04a0dd7ac7ccb2532aa4843d1dd21f9d46f1db6f
Garrett <52760019+Casimir255@users.noreply.github.com>
2025-12-30 15:20:24 -0600

    Staged

---
+++ Torch2API/DTOs/Logs/LogLine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Torch2API.DTOs.Logs
+{
+    public class LogLine
+    {
+        public string InstanceName { get; set; }
+        public string Level { get; set; }
+        public string Message { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
